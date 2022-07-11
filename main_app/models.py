cab3939f9e668e9fb21cbaf21ef835aad3bf8cbe
“julian <badjab326@gmail.com>
2022-07-11 12:08:12 -0700

    first commit

---
+++ main_app/models.py
@@ -0,0 +1,52 @@
+from django.db import models
+from django.urls import reverse
+from datetime import date
+from django.contrib.auth.models import User
+
+
+# Create your models here.
+
+TIMES = (
+    ('M', 'Morning'),
+    ('A', 'Afternoon'),
+    ('E', 'Evening')
+)
+
+class Exercise(models.Model):
+  name = models.CharField(max_length=50)
+  sets = models.IntegerField()
+  reps = models.CharField(max_length=50)
+  user = models.ForeignKey(User, on_delete=models.CASCADE)
+
+  def __str__(self):
+    return f'{self.name} {self.sets} {self.reps}'
+
+  def get_absolute_url(self):
+    return reverse('exercises_detail', kwargs={'pk': self.id})
+
+class Routine(models.Model):
+    name = models.CharField(max_length=100)
+    muscle_group = models.CharField(max_length=100)
+    description = models.TextField(max_length=250)
+    days_per_week = models.IntegerField()
+
+    def __str__(self):
+        return self.name
+    
+    def get_absolute_url(self):
+        return reverse('detail', kwargs={'routine_id': self.id})
+
+class Doing(models.Model):
+    date = models.DateField('workout date')
+    time = models.CharField(
+        max_length=1,
+        choices=TIMES,
+        default=TIMES[0][0]
+    )
+    routine = models.ForeignKey(Routine, on_delete=models.CASCADE)
+
+    def __str__(self):
+        return f"{self.get_time_display()} on {self.date}"
+
+    class Meta:
+        ordering = ['-date']
---
+++ main_app/views.py
@@ -0,0 +1,31 @@
+from django.shortcuts import render
+from django.views.generic.edit import CreateView, UpdateView, DeleteView
+from .models import Routine
+
+# Create your views here.
+def home(request):
+  return render(request, 'home.html')
+  
+def about(request):
+  return render(request, 'about.html')
+
+def routines_index(request):
+  routines = Routine.objects.all()
+  return render(request, 'routines/index.html', { 'routines': routines })
+
+def routines_detail(request, routine_id):
+  routine = Routine.objects.get(id=routine_id)
+  return render(request, 'routines/detail.html', { 'routine': routine })
+
+class RoutineCreate(CreateView):
+  model = Routine
+  fields = '__all__'
+  success_url = '/routines/'
+
+class RoutineUpdate(UpdateView):
+  model = Routine
+  fields = '__all__'
+
+class RoutineDelete(DeleteView):
+  model = Routine
+  success_url = '/routines/'
